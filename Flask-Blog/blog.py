5462dee353f8d9520a0320ee9c3f4652492f7624
Pampipampupampa <jeremybois@rocketmail.com>
2015-04-25 14:05:23 +0200

    Basic structure for the Flask blog

--- Flask-Blog/blog.py
+++ Flask-Blog/blog.py
@@ -35,6 +35,16 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+
 if __name__ == '__main__':
     # Start the development server in debug mode
     app.run(debug=True)
