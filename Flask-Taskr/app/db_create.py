039797e8cf89156bf96fd12c1c8a0dcfe52d71c0
Pampipampupampa <jeremybois@rocketmail.com>
2015-04-27 22:37:33 +0200

    Last commit on the Flask-Taskr project part 1, Date format stil not respected but don’t know why. Everyting else is working

--- Flask-Taskr/app/db_create.py
+++ Flask-Taskr/app/db_create.py
@@ -13,12 +13,15 @@ if __name__ == '__main__':
 
         cur = connection.cursor()
 
+        # Delete database table if exist
+        cur.execute("DROP TABLE if exists tasks")
+
         cur.execute("""CREATE TABLE tasks (task_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name TEXT NOT NULL, due_date TEXT NOT NULL,
                     priority INTEGER NOT NULL, status INTEGER NOT NULL)""")
 
         cur.execute("""INSERT INTO tasks (name, due_date, priority, status)
-                    VALUES("Finish this tutorial", "02/03/2014", 10, 1)""")
+                    VALUES("Finish this tutorial", "03/02/2014", 10, 1)""")
 
         cur.execute("""INSERT INTO tasks (name, due_date, priority, status)
-                    VALUES("Finish Real Python Course 2", "02/03/2014", 10, 1)""")
+                    VALUES("Finish Real Python Course 2", "03/02/2014", 10, 1)""")
