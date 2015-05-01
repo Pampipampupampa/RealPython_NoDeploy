a40f69586efbc3c9ed6a45851ff8015f28769754
Pampipampupampa <jeremybois@rocketmail.com>
2015-05-01 20:36:14 +0200

    Prepare folder for chapter instruction with Flask-Taskr2 files

---
+++ Flask-Taskr3/app/_config.py
@@ -0,0 +1,20 @@
+# -*- coding:Utf8 -*-
+
+import os
+
+########################
+#    Main Program :    #
+########################
+
+# Grabs the folder where the script runs
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = "flask-taskr.db"
+WTF_CSRF_ENABLED = True
+SECRET_KEY = "secret"
+
+# Defines the full path of the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# Database URI
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
