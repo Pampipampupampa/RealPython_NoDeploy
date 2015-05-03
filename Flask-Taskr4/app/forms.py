72a8a8dcbe2e26602e020c18aa917563008d3950
Pampipampupampa <jeremybois@rocketmail.com>
2015-05-03 18:03:48 +0200

    Prepare folder for course

---
+++ Flask-Taskr4/app/forms.py
@@ -0,0 +1,50 @@
+# -*- coding:Utf8 -*-
+
+
+"""
+    Flask from handler and checker
+"""
+
+from flask_wtf import Form
+from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
+from wtforms.validators import InputRequired, Length, EqualTo, Email
+
+
+class AddTaskForm(Form):
+    """
+        Create a form handler/validator
+    """
+    # task_id = IntegerField('Priority')
+    name = StringField('Task Name', validators=[InputRequired(), ])
+    due_date = DateField('Due Date (dd/mm/yyyy)', validators=[InputRequired(), ],
+                         format='%d/%m/%Y')
+    priority = SelectField('Priority', validators=[InputRequired(), ],
+                           choices=[('1', '1'), ('2', '2'), ('3', '3'),
+                                    ('4', '4'), ('5', '5'), ('6', '6'),
+                                    ('7', '7'), ('8', '8'), ('9', '9'),
+                                    ('10', '10')])
+    status = IntegerField('Status')
+
+
+class RegisterForm(Form):
+    """
+        Create a form handler/validator for registration
+    """
+    name = StringField('Username', validators=[InputRequired(),
+                       Length(min=6, max=25)])
+    email = StringField('Email', validators=[InputRequired(), Email(),
+                                             Length(min=6, max=40)])
+    password = PasswordField('Password', validators=[InputRequired(),
+                                                     Length(min=6, max=40)])
+    confirm = PasswordField('Password',
+                            validators=[InputRequired(),
+                                        EqualTo('password',
+                                                message='Passwords must match')])
+
+
+class LoginForm(Form):
+    """
+        Create a form handler/validator for Login
+    """
+    name = StringField('Username', validators=[InputRequired(), ])
+    password = PasswordField('Password', validators=[InputRequired(), ])
